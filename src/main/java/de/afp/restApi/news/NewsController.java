68c1c14a1dc7ef19810b9101a987e4493f02a065
domrr <dr@afp-akademie.de>
2021-05-31 15:35:51 +0200

    update NewsController

---
+++ src/main/java/de/afp/restApi/news/NewsController.java
@@ -0,0 +1,51 @@
+package de.afp.restApi.news;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/news")
+public class NewsController {
+
+    public final NewsService NEWSSERVICE;
+
+    @Autowired
+    public NewsController(NewsService NEWSSERVICE) {
+        this.NEWSSERVICE = NEWSSERVICE;
+    }
+
+    @PostMapping(path = "/add")
+    public ResponseEntity<NewsModel> newsAnlegen(@RequestBody NewsModel news) {
+        NewsModel n = NEWSSERVICE.newsAnlegen(news);
+        return new ResponseEntity<>(n, HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/all")
+    public ResponseEntity<List<NewsModel>> alleNews() {
+        List<NewsModel> ls = NEWSSERVICE.alleNews();
+        return new ResponseEntity<>(ls, HttpStatus.OK);
+    }
+
+    @PutMapping(path = "/update")
+    public ResponseEntity<NewsModel> newsUpadta(@RequestBody NewsModel news) {
+        NewsModel n = NEWSSERVICE.newsAnlegen(news);
+        return new ResponseEntity<>(n, HttpStatus.OK);
+    }
+
+    @DeleteMapping(path = "/delete/{id}")
+    public ResponseEntity<?> newsLoeschen(@PathVariable("id") int id) {
+        NEWSSERVICE.newsLoeschen(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
