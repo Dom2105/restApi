9a097f32f872336688ff293c2f1d2dad20ba990e
domrr <dr@afp-akademie.de>
2021-05-31 13:49:14 +0200

    add newsmodel

---
+++ src/main/java/de/afp/restApi/news/NewsModel.java
@@ -0,0 +1,55 @@
+package de.afp.restApi.news;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "news")
+public class NewsModel implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int newsId;
+    @Column(columnDefinition = "varchar(25)")
+    private String titel;
+    private String text;
+
+    public NewsModel() {
+    }
+
+    public NewsModel(String titel, String text) {
+        this.titel = titel;
+        this.text = text;
+    }
+
+    public int getNewsId() {
+        return this.newsId;
+    }
+
+    public void setNewsId(int newsId) {
+        this.newsId = newsId;
+    }
+
+    public String getTitel() {
+        return this.titel;
+    }
+
+    public void setTitel(String titel) {
+        this.titel = titel;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+}
