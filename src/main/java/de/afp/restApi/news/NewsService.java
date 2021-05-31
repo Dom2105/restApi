8ee09a42a476de94c2d6f83826396df2f9e768ad
domrr <dr@afp-akademie.de>
2021-05-31 14:25:27 +0200

    add NewsRepo and NewsService

--- src/main/java/de/afp/restApi/news/NewsService.java
+++ src/main/java/de/afp/restApi/news/NewsService.java
@@ -1,5 +1,44 @@
 package de.afp.restApi.news;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class NewsService {
+
+    private final NewsRepo NEWSREPO;
+
+    @Autowired
+    public NewsService(NewsRepo NEWSREPO) {
+        this.NEWSREPO = NEWSREPO;
+    }
+
+    public List<NewsModel> alleNews() {
+        return NEWSREPO.findAll();
+    }
+
+    public NewsModel newsAnlegen(NewsModel news) {
+        return NEWSREPO.save(news);
+    }
+
+    public NewsModel newsUpdate(NewsModel news) {
+        return NEWSREPO.save(news);
+    }
+
+    public String newsLoeschen(int id) {
+        String text;
+
+        try {
+            NEWSREPO.deleteById(id);
+            text = "News mit der ID " + id + " wurde entfernt!";
+        } catch (Exception e) {
+            System.out.println(e);
+            text = "News mit der ID " + id + " konnte nicht gelöscht werden";
+        }
+
+        return text;
+    }
 
 }
