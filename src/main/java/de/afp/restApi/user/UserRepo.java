f668e87cd6df170c6e2d8d8cde77e695e5167609
domrr <dr@afp-akademie.de>
2021-06-02 09:23:13 +0200

    update User/Repo/Model/Controller

--- src/main/java/de/afp/restApi/user/UserRepo.java
+++ src/main/java/de/afp/restApi/user/UserRepo.java
@@ -1,13 +1,7 @@
 package de.afp.restApi.user;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import de.afp.restApi.news.NewsModel;
 
 @Repository
 public interface UserRepo extends JpaRepository<UserModel, Integer> {
