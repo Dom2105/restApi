4626d0231e75bb2cd1f085fc32c712b5d63e0ce6
domrr <dr@afp-akademie.de>
2021-06-02 12:08:38 +0200

    update newsService and UserController

--- src/main/java/de/afp/restApi/news/NewsService.java
+++ src/main/java/de/afp/restApi/news/NewsService.java
@@ -19,8 +19,24 @@ public class NewsService {
         return NEWSREPO.findAll();
     }
 
+    /* public static int teil = 1; */
+
     public NewsModel newsAnlegen(NewsModel news) {
-        return NEWSREPO.save(news);
+        int teil = 1;
+        if (news.getText().length() <= 255) {
+            return NEWSREPO.save(news);
+        } else {
+            String oldTitle = news.getTitel();
+            String newTitle = news.getTitel() + " Teil-" + teil++;
+            String newText = news.getText().substring(0, 255);
+            String rest = news.getText().substring(255);
+            news.setTitel(newTitle);
+            news.setText(newText);
+            /* NewsModel news2 = new NewsModel(); */
+            newsAnlegen(new NewsModel(oldTitle, rest, news.getUser()));
+            return NEWSREPO.save(news);
+        }
+
     }
 
     public NewsModel newsUpdate(NewsModel news) throws Exception {
